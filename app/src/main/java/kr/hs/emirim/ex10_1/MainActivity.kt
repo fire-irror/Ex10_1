0dcf299b6fac522a96246f303027d331fc2af7d9
fire-irror <firenoah.m@gmail.com>
2023-09-12 20:05:30 +0900

    실습 10-1 엑티비티 전환 완성

--- app/src/main/java/kr/hs/emirim/ex10_1/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/ex10_1/MainActivity.kt
@@ -4,19 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnMain : Button
-
+    lateinit var radioGroup : RadioGroup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var intent: Intent
 
         btnMain = findViewById(R.id.btnMain)
-        btnMain.setOnClickListener{
-            var intent = Intent(applicationContext, SecondActivity::class.java) //내가 시작하고 싶은 엑티브티 이름을 쓰면 된다
-            startActivity(intent)
+        radioGroup = findViewById(R.id.radioGroup)
+        intent = Intent(applicationContext, SecondActivity::class.java)
 
+        btnMain.setOnClickListener{
+            if(radioGroup.checkedRadioButtonId == R.id.radioButton){
+                intent = Intent(applicationContext, SecondActivity::class.java)
+            }else
+                intent = Intent(applicationContext,ThirdActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/kr/hs/emirim/ex10_1/ThirdActivity.kt
@@ -0,0 +1,18 @@
+package kr.hs.emirim.ex10_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ThirdActivity : AppCompatActivity() {
+    lateinit var btnThird: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+
+        btnThird = findViewById(R.id.btnThird)
+        btnThird.setOnClickListener {
+            finish()
+        }
+    }
+}
